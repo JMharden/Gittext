1ec9790bacfcb8c2f505b9ba48b6de483eed98a1
zhuhangan <zhuhangan@qiyi.com>
2019-03-08 17:00:52 +0800

    佣金

--- game/Application/Api/service/DrawService.php
+++ game/Application/Api/service/DrawService.php
@@ -50,6 +50,7 @@ class CommissionService
            throw new Exception("佣金余额不足");
        }
         $data=['user_id'=>$userId,
+        $data=['user_id'=>$userId,
                 'create_time'=>NOW_TIME,
                 'money'=>$amount,
                'type'=>1
