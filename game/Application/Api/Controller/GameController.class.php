9aed70af3a7db367afa9b52577666d564b535917
game <34573703+JMharden@users.noreply.github.com>
2019-05-09 17:59:27 +0800

    Update GameController.class.php

--- game/Application/Api/Controller/GameController.class.php
+++ game/Application/Api/Controller/GameController.class.php
@@ -57,6 +57,30 @@ class GameController extends ApiController
             echo json_encode(['status' => '-1', 'msg' => $e->getMessage()]);
         }
     }
+       public function createFunMatch(){
+       try{
+            $playUser =  $_GET['playUser'];
+            $gameService =  new GameService();
+            $data = $gameService->createFunMatch($playUser);
+            echo json_encode(['status' => '1', 'msg' => '返回成功', 'data' => $data]);
+        }catch (Exception  $e){
+            echo json_encode(['status' => '-1', 'msg' => $e->getMessage()]);
+        }
+    }
+    public function funGameSettle(){
+        try{
+          //  ($matchId, $result, $winner, $winnerId)
+            $matchId =  $_POST['matchId'];
+            $winner =  $_POST['winner'];
+            $winnerId =  $_POST['winnerId'];
+            $result =   $_POST['result'];
+            $gameService =  new GameService();
+            $data = $gameService->funGameSettle($matchId, $result, $winner, $winnerId);
+            echo json_encode(['status' => '1', 'msg' => '返回成功', 'data' => $data]);
+        }catch (Exception  $e){
+            echo json_encode(['status' => '-1', 'msg' => $e->getMessage()]);
+        }
+    }
 
 
 }
