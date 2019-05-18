48f2827e3a6fd6a22144292695b81bc0ee365946
game <34573703+JMharden@users.noreply.github.com>
2019-05-18 16:44:01 +0800

    日志写入
    日志写入

--- game/Application/Api/Controller/ApiController.class.php
+++ game/Application/Api/Controller/ApiController.class.php
@@ -1,10 +1,12 @@
 <?php
 namespace Api\Controller;
 use Think\Controller;
-
+use Api\Service\GameService;
 
 class ApiController extends Controller {
-     public function _initialize_bak()
+
+     public function _initialize()
+
      {
      	 $this->_load_config();
          $token = $_POST['token'];
@@ -14,6 +16,17 @@ class ApiController extends Controller {
          }
         //  var_dump(S($token));
          //exit;
+
+        $uid =S($token)[2];
+         // $uid = 182;
+        $useInfo =S('user_info_'.$uid);
+        if(!$useInfo){
+             $user  = M('user')->where(array('id'=>$uid))->field('id,nickname,money,rank,headimg,club_id,empiric,is_club_owner,match_amount,win_amount')->find();
+             // $scene = M('play_log')->where(array('user_id'=>$uid))->count();
+             // $win   = M('play_log')->where(array('user_id'=>$uid,'result'=>'赢'))->count();
+             $probability =round($user['win_amount']/$user['match_amount']*100,2)."%";  //胜率
+             // $grade = $this->grade($win);
+
          $uid =S($token)[2];
          $useInfo =S('user_info_'.$uid);
         if(!$useInfo){
@@ -22,6 +35,7 @@ class ApiController extends Controller {
              $win   = M('play_log')->where(array('user_id'=>$uid,'result'=>'赢'))->count();
              $probability =round($win/$scene*100,2)."%";
              $grade = $this->grade($win);
+
              $userInfo = array(
                  'id'       => $user['id'] ,
                  'openid'    =>$user['openid'],
@@ -31,9 +45,15 @@ class ApiController extends Controller {
                  'money'   => $user['money'],
                  'headimg'  => $user['headimg'],
                  'empiric'  => $user['empiric'],//经验值
+
+                 'rank'    => $user['rank'],//段位
+                 'level'    =>  GameService::getDuan($user['rank']),
+
                  'active' => $user['active_point'],//活跃度
                  'inter'    => $user['integration'],//积分
                  'grade'    => $grade,//段位
+                 'mactch_amount' => $user['match_amount'],//总场次
+                 'most_step'    => 12321,//最高步数
                  'probability' =>$probability,//胜率
              );
             if(!$userInfo){
@@ -45,11 +65,12 @@ class ApiController extends Controller {
             $GLOBALS['current_use_info'] =$userInfo;
         }
          $GLOBALS['current_uid'] =$uid;
-
      }
+ }
+
+
         // 加载配置
-    protected function _load_config()
-    {
+    protected function _load_config(){
         $_CFG = S('sys_config');
         if (empty($_CFG) || APP_DEBUG) {
             $config = M('config')->select();
@@ -84,6 +105,7 @@ class ApiController extends Controller {
  */
     public function login()
     {
+      
         session_start();
         // 开发者使用登陆凭证 code 获取 session_key 和 openid
         // $APPID = 'wx1234d2031a772642';//自己配置
@@ -137,14 +159,17 @@ class ApiController extends Controller {
         $user_info['session3rd'] = $session3rd;
 
         $user = M('user')->where(array('openid'=>$user_info['openId']))->find();
+        $slime = implode(',', $user['slime']);
         $time = date('Y-m-d H:i:s',time());
         // if (session('wechat_info.openid')) {
            
             if($user){//老用户
+
                 $user_info['club_id'] = (int)$user['club_id'];
                 $user_info['avatarUrl'] = $user['headimg'];
                 $user_info['active_point'] = (int)$user['active_point'];
                 $user_info['money'] = $user['money'];
+                $user_info['slime'] = $slime[0];
                 $user_info['id'] = (int)$user['id'];
                 $loginNum = $user['all_login_time']+1;
                 M('user')->where(array('openid'=>$user_info['openId']))->save(array('last_login_time'=>$time,'all_login_time'=>$loginNum));
@@ -160,11 +185,11 @@ class ApiController extends Controller {
                 // var_dump($user_info);exit;
                 //获取推荐关系
                 if($uid){
-                    $introducer_2id = M('user')->where(array('id'=>$uid))->getField('introducer_id');//推荐人的上级用户ID
-                    $introducer_3id = M('user')->where(array('id'=>$introducer_2id))->getField('introducer_id');//推荐人的上级用户ID
-                    $user_data['introducer_id'] = $uid;
-                    $user_data['introducer_2id'] = $introducer_2id;
-                    $user_data['introducer_3id'] = $introducer_3id;
+                    $parent = M('user')->where(array('id'=>$uid))->field('parent1,parent2')->find();//推荐人的上两级级用户ID
+                    // $parent3 = M('user')->where(array('id'=>$parent2))->getField('parent1');//推荐人的上级用户ID
+                    $user_data['parent1'] = $uid;
+                    $user_data['parent2'] = $parent['parent1'];
+                    $user_data['parent3'] = $parent['parent2'];
                
                     M('relation')->add(array(
                         'openid' => $user_data['openid'],
@@ -224,6 +249,122 @@ class ApiController extends Controller {
 
         return substr($result, 0, $len);
     }
+
+	// public function index()
+ //    {   
+ //       // echo "123123";exit;
+ //        $token = $_POST['token'];
+
+ //        if ($token == null || S($token) == null) {
+ //            echo json_encode(['status' => '403', 'message' => 'request forbidden']);
+ //            exit;
+ //        }
+ //        // var_dump(S($token));exit;
+ //        $uid =S($token)[2];
+ //       if(S('user_info_'.$uid)){
+
+ //           // $userInfo = M('user')->where(array('id'=>$uid))->find();
+ //            $user  = M('user')->where(array('id'=>$uid))->field('id,nickname,money2,headimg,integration,empiric,active_point')->find();
+ //            $scene = M('play_log')->where(array('user_id'=>$uid))->count();
+ //            $win   = M('play_log')->where(array('user_id'=>$uid,'result'=>'赢'))->count();
+ //            $probability =round($win/$scene*100,2)."%";
+
+ //            $grade = $this->grade($win);
+ //            $userInfo = array(
+ //                'id'       => $user['id'] ,
+ //                'openid'    =>$user['openid'],
+ //                'club_id'    =>$user['club_id'],
+ //                'club_role'    =>$user['club_role '],
+ //                'nickname' => $user['nickname'],
+ //                'money'   => $user['money'],
+ //                'headimg'  => $user['headimg'],
+ //                'empiric'  => $user['empiric'],//经验值
+ //                'active' => $user['active_point'],//活跃度
+ //                'inter'    => $user['integration'],//积分
+ //                'grade'    => $grade,//段位
+ //                'probability' =>$probability,//胜率
+                
+ //            );
+       
+ //           if(!$userInfo){
+ //               echo json_encode(['status' => '403', 'msg' => 'userInfo not find']);
+ //               exit;
+ //           }
+ //           //后面有接口要取用户信息（推荐关系啥的）直接从缓存里拿就行
+ //           S('user_info_'.$uid, $userInfo,18000);//用户信息存入Redis
+ //       }
+ //    }
+
+    public function upload(){
+    	$this->display();
+    }
+
+       public function getUrl(){
+        $URL['PHP_SELF'] = isset($_SERVER['PHP_SELF']) ? $_SERVER['PHP_SELF'] : (isset($_SERVER['SCRIPT_NAME']) ? $_SERVER['SCRIPT_NAME'] : $_SERVER['ORIG_PATH_INFO']);   //当前页面名称
+
+
+        $URL['DOMAIN'] = $_SERVER['HTTP_HOST'];  //域名(主机名)
+
+
+        $URL['QUERY_STRING'] = $_SERVER['QUERY_STRING'];   //URL 参数
+
+
+        $URL['URI'] = $URL['PHP_SELF'].($URL['QUERY_STRING'] ? "?".$URL['QUERY_STRING'] : "");
+
+
+        $URL['URL'] = "http://".$URL['DOMAIN'].$URL['PHP_SELF'].($URL['QUERY_STRING'] ? "?".$URL['QUERY_STRING'] : ""); //完整URL地址
+        return $URL['URL'] ? $URL['URL'] : "unknow";
+    }
+
+
+    public function getPostData() {
+         $postdata = file_get_contents("php://input");
+         $data = urldecode($postdata);
+         $data = substr_replace($data, '', 0, 5);
+         $data = str_replace(PHP_EOL, '', $data);
+         //var_dump($data);
+         //$data = json_decode($data, true);
+        //var_dump($data);
+          return $data;
+    }
+
+
+    public function getIps() //获取用户IP
+    {
+       if(getenv('HTTP_CLIENT_IP') && strcasecmp(getenv('HTTP_CLIENT_IP'), 'unknown'))
+       {
+        $IP = getenv('HTTP_CLIENT_IP');
+       } elseif(getenv('HTTP_X_FORWARDED_FOR') && strcasecmp(getenv('HTTP_X_FORWARDED_FOR'), 'unknown')) {
+        $IP = getenv('HTTP_X_FORWARDED_FOR');
+       } elseif(getenv('REMOTE_ADDR') && strcasecmp(getenv('REMOTE_ADDR'), 'unknown')) {
+        $IP = getenv('REMOTE_ADDR');
+       } elseif(isset($_SERVER['REMOTE_ADDR']) && $_SERVER['REMOTE_ADDR'] && strcasecmp($_SERVER['REMOTE_ADDR'], 'unknown')) {
+        $IP = $_SERVER['REMOTE_ADDR'];
+       }
+       return $IP ? $IP : "unknow";
+    }
+    //日志写入
+    public function write_log(){ 
+        $data = json_encode(array(date('Y-m-d H:i:s'),$this->getIps(),$this->getUrl(),$this->getPostData()));
+
+        $years = date('Y-m');
+        //设置路径目录信息
+        $url = './Public/log/'.$years.'/'.date('Ymd').'_log.txt';  
+          // var_dump($url);exit;
+        $dir_name=dirname($url);
+          //目录不存在就创建
+          if(!file_exists($dir_name))
+          {
+            //iconv防止中文名乱码
+           $res = mkdir(iconv("UTF-8", "GBK", $dir_name),0777,true);
+          }
+          $fp = fopen($url,"a");//打开文件资源通道 不存在则自动创建   
+
+        fwrite($fp,var_export($data,true)."\r\n");//写入文件
+        fclose($fp);//关闭资源通道
+    }
+
+
 
 
 
