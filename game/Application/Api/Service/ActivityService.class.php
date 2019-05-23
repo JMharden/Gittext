1395781606558b21246e1a6b17d2507f830476cf
zhuhangan <zhuhangan@qiyi.com>
2019-05-23 11:42:32 +0800

    奖励7

--- game/Application/Api/Service/ActivityService.class.php
+++ game/Application/Api/Service/ActivityService.class.php
@@ -29,26 +29,42 @@ class ActivityService
             date('w');
             date_add() ;
             $maxAccDay=1;
-           $data= M('login_reward')->where(array("user_id" => $userId,"create_date"=>array('GT',$start)))->order('create_date desc')->select();
-          //判断当天是否已经登陆过
-           if($data&& $data[0]['create_date']==date("Y-m-d")){
-               return  $data;
-           }else if($data){
-               $maxAccDay = $data[0]['accu_login_days']+1;
-           }
-          $reward =  $this->getLoginRewardList($maxAccDay);
-           $addData =[
-                "user_id"=>$userId,
-                'accu_login_days'=>$maxAccDay,
-                'reward_num'=>$reward['num'],
-               'reward_type'=>$reward['type'],
-                "create_date"=>date("Y-m-d"),
-                'expire_date'=>$end,
-                'is_draw'=>'N'
-            ];
-            $last = M('login_reward')->add($addData);
-            $addData['id']=$last;
-            return array_merge($addData,$data);
+            $data= M('login_reward')->where(array("user_id" => $userId,"create_date"=>array('GT',$start)))->order('create_date desc')->select();
+            //判断当天是否已经登陆过
+            if($data&& $data[0]['create_date']==date("Y-m-d")){
+                $maxAccDay = $data[0]['accu_login_days'];
+            }else {
+                if($data){
+                    $maxAccDay = $data[0]['accu_login_days']+1;
+                }
+                $reward =  $this->getLoginRewardList($maxAccDay);
+                $addData =[
+                    "user_id"=>$userId,
+                    'accu_login_days'=>$maxAccDay,
+                    'reward_num'=>$reward['num'],
+                    'reward_type'=>$reward['type'],
+                    "create_date"=>date("Y-m-d"),
+                    'expire_date'=>$end,
+                    'is_draw'=>'N'
+                ];
+                $last = M('login_reward')->add($addData);
+                $addData['id']=$last;
+                $data = array_merge($addData,$data);
+            }
+            //就剩余活动天数的数据返回
+            while($maxAccDay<7){
+                $maxAccDay=$maxAccDay+1;
+                $dayreward = $this->getLoginRewardList($maxAccDay);
+                $list[]=[
+                    'accu_login_days'=>$maxAccDay,
+                    'reward_num'=>$dayreward['num'],
+                    'reward_type'=>$dayreward['type'],
+                    'status'=>'N'
+                ];
+            }
+
+            return  array_merge($data,$list);
+
         }
         return null;
     }
@@ -80,7 +96,7 @@ class ActivityService
     /**
      * 累计登陆领取逻辑
      * @param $userId
-      * @param $activityId    对应login_reward表中的主键id
+     * @param $activityId    对应login_reward表中的主键id
      * @return array|null
      */
     function fetchLoginReward($userId,$activityId)
