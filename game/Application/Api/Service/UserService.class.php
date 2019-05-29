e357cf1f0c765a791373e3a085954a6bab7e9b5e
game <34573703+JMharden@users.noreply.github.com>
2019-05-29 18:59:46 +0800

    用户信息完善

--- game/Application/Api/Service/UserService.class.php
+++ game/Application/Api/Service/UserService.class.php
@@ -17,20 +17,21 @@ class UserService
      * @param $user
      * @return mixed|null
      */
-    function  addUser($user){
-        if($user){
+    function  addUser($user,$openid){
+        if(!$openid){
             return null;
         }
           $userId =  M('user_base')->add($user);
           $userExtr =[
             'user_id'=>$userId,
-            'create_time'=>time()
+            'create_time'=>time(),
+            'openid'    =>$openid
           ];
            M('user')->add($userExtr);
             //清除缓存
            S('user_info_base' . $userId,null);
            S('user_info_full' . $userId,null);
-           $user['id']=$userId;
+          // $user = UserService::getUserFullInfo($userId);           // $user['id']=$userId;
           return $user;
     }
 
@@ -48,7 +49,7 @@ class UserService
             } else {
                 $user = M('user_base')->where(array('id' => $userId))->find();
                 if($user){
-                  $openid = $user['openId'];
+                  $openid = $user['openid'];
                     S('user_info_base' .$openid , $userInfo, 18000);//用户信息存入Redis
                 }
                 S('user_info_base' . $userId, $userInfo, 18000);//用户信息存入Redis
@@ -64,7 +65,7 @@ class UserService
             if ($userInfo) {
                 return $userInfo;
             } else {
-                $user = M('user_base')->where(array('openId' => $openId))->find();
+                $user = M('user_base')->where(array('openid' => $openId))->find();
                 if($user){
                     $userId = $user['userId'];
                     S('user_info_base' . $userId, $userInfo, 18000);//用户信息存入Redis
@@ -80,11 +81,25 @@ class UserService
         if ($openId) {
 
             $userBase = $this->getUserBaseInfoByOpen($openId);
+            // var_dump($userBase);exit;
             if ($userBase) {
-                $userId = $userBase['user_id'];
-                $userExtr = M('user')->where(array('user_id' => $userId))->find();
+                $openid = $userBase['openid'];
+                $userExtr = M('user')->where(array('openid' => $openid))->find();
+                $rank = GameService::getDuan($userExtr['rank']);
+                $userInfo = [
+                    'is_club_owner' => $userExtr['is_club_owner'],
+                    'money' => $userExtr['money'],
+                    'slimeIndex' => 0,
+                    'club_id' => $userExtr['club_id'],
+                    'advert' => $userExtr['advert'],
+                    'stamina' => $userExtr['stamina'],
+                    'rank'    => $rank['level'],
+                    'ranks'   => $rank['max'] - $rank['min'],
+                    'rankNum'   => $userExtr['rank'] -$rank['min']
+                ];
+
                 if($userExtr){
-                    return array_merge($userBase,$userExtr);
+                    return array_merge($userBase,$userInfo);
                 }else{
                     return $userBase;
                 }
@@ -103,8 +118,20 @@ class UserService
             $userBase = $this->getUserBaseInfo($userId);
             if ($userBase) {
                 $userExtr = M('user')->where(array('user_id' => $userId))->find();
+                $rank = GameService::getDuan($userExtr['rank']);
+                $userInfo = [
+                    'is_club_owner' => $userExtr['is_club_owner'],
+                    'money' => $userExtr['money'],
+                    'slimeIndex' => 0,
+                    'club_id' => $userExtr['club_id'],
+                    'advert' => $userExtr['advert'],
+                    'stamina' => $userExtr['stamina'],
+                    'rank'    => $rank['level'],
+                    'ranks'   => $rank['max'] - $rank['min'],
+                    'rankNum'   => $userExtr['rank'] -$rank['min']
+                ];
                 if($userExtr){
-                    $userFull =  array_merge($userBase,$userExtr);
+                    $userFull =  array_merge($userBase,$userInfo);
                 }else{
                     $userFull = $userBase;
                 }
